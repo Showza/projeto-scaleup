30a8ef12e7b6ccc8aa1b314ccaed6e0ec1bfbc29
gabrielcaetanodm <gabrielcaetanodm@gmail.com>
2019-01-09 19:37:01 -0200

    arquivos para montagem do frontend

---
+++ application/views/frontend/template/footer.php
@@ -0,0 +1,18 @@
+</div>
+<!-- /.row -->
+
+<hr>
+
+<!-- Footer -->
+<footer>
+    <div class="row">
+        <div class="col-lg-12">
+            <p>Copyright &copy; Desenvolvido por Voluta Soluções Digitais</p>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+</footer>
+
+</div>
+<!-- /.container -->
