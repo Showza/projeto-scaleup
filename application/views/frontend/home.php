6637e1ea24496f55a97b7bae476635ca33a77a5e
gabrielcaetanodm <gabrielcaetanodm@gmail.com>
2019-01-11 15:13:41 -0200

    template frontend incompleto (palpite)

--- application/views/frontend/home.php
+++ application/views/frontend/home.php
@@ -3,21 +3,18 @@
 
   <!-- Full-width images with number and caption text -->
   <div class="mySlides">
-    <div class="numbertext">1 / 3</div>
     <img src="<?php echo base_url('assets/frontend/img/slider/slider1.jpg') ?>" style="width:100%">
     <font face="monospace"><div class="text">União, Sinergia e Paixão.</div></font>
     <font face="monospace"><div class="subtext">Conheça nossa equipe.</div></font>
   </div>
 
   <div class="mySlides">
-    <div class="numbertext">2 / 3</div>
     <img src="<?php echo base_url('assets/frontend/img/slider/slider2.jpg') ?>" style="width:100%">
     <font face="monospace"><div class="text">Digite algo aqui!</div></font>
     <font face="monospace"><div class="subtext">Mais um texto!</div></font>
   </div>
 
   <div class="mySlides">
-    <div class="numbertext">3 / 3</div>
     <img src="<?php echo base_url('assets/frontend/img/slider/slider3.jpg') ?>" style="width:100%">
     <font face="monospace"><div class="text">Escreva algo!</div></font>
     <font face="monospace"><div class="subtext">Esvreva mais alguma coisa.</div></font>
@@ -30,8 +27,45 @@
 <br>
 
 <!-- The dots/circles -->
-<div style="text-align:center">
+<!-- <div style="text-align:center">
   <span class="dot" onclick="currentSlide(1)"></span>
   <span class="dot" onclick="currentSlide(2)"></span>
   <span class="dot" onclick="currentSlide(3)"></span>
+</div> -->
+
+<div class="container-fluid wrapper" style="background-color:#2E2E2E; height:250px;">
+        <div style="float: left; width: 45%; text-align:center;">
+            <p style="height: 85px">
+                <font face="monospace" color="#228B22"><h1><b>Quem somos nós</b></h1></font>
+            </p>
+        </div>
+        <div style="float: right; width: 45%; text-align:left">
+            <p style="height: 70px">
+                <font face="monospace" color="#f2f2f2">
+                    <h3>A ScaleUp Jr é uma empresa composta por estudantes do curso de Química Industrial e Química Licenciatura da UFOP</h3>
+                </font>
+            </p>
+        </div>
+</div>
+<br>
+
+<div class="container-fluid wrapper" style="background-color:#2E2E2E; height:250px;">
+        <div style="float:left; padding:2px; text-align:center">
+            <p style="height: 85px">
+                <font face="monospace" color="#228B22"><h1><b>Nossa missão</b></h1></font>
+                <p>
+                    <font face="monospace" color="#f2f2f2">Transformar pessoas, provendo soluções que valorizem nossos clientes</font>
+                </p>
+            </p>
+        </div>
+        <div style="float:right; padding:2px; text-align:center">
+            <p style="height: 85px">
+                <font face="monospace" color="#228B22"><h1><b>Nossa visão</b></h1></font>
+            </p>
+        </div>
+        <div style="float:center; padding:2px; text-align:center">
+            <p style="height: 85px">
+                <font face="monospace" color="#228B22"><h1><b>Nossos valores</b></h1></font>
+            </p>
+        </div>
 </div>
