5df1ad25dfb5af1e9f23e85747a4e4f9973e2e6e
gabrielcaetanodm <gabrielcaetanodm@gmail.com>
2019-01-11 17:07:08 -0200

    template frontend incompleto (serviços e quem somos atualizado)

--- application/views/frontend/home.php
+++ application/views/frontend/home.php
@@ -21,8 +21,8 @@
   </div>
 
   <!-- Next and previous buttons -->
-  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+  <a class="prev" onclick="plusSlides(-1)" style="font-size:300%;">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)" style="font-size:300%;">&#10095;</a>
 </div>
 <br>
 
@@ -33,10 +33,13 @@
   <span class="dot" onclick="currentSlide(3)"></span>
 </div> -->
 
+<!-- Coluna Quem Somos -->
 <div class="container-fluid wrapper" style="background-color:#2E2E2E; height:250px;">
         <div style="float: left; width: 45%; text-align:center;">
-            <p style="height: 85px">
-                <font face="monospace" color="#228B22"><h1><b>Quem somos nós</b></h1></font>
+            <p style="height: 70px">
+                <font face="monospace" color="#9aff01">
+                    <h1 style="font-size:400%;"><b>Quem somos nós</b></h1>
+                </font>
             </p>
         </div>
         <div style="float: right; width: 45%; text-align:left">
@@ -49,23 +52,108 @@
 </div>
 <br>
 
-<div class="container-fluid wrapper" style="background-color:#2E2E2E; height:250px;">
+<!-- Coluna de Missão, Visão e Valores -->
+<div class="container-fluid wrapper" style="background-color:#2E2E2E; height:350px;">
+    <!-- Coluna da Direita -->
         <div style="float:left; padding:2px; text-align:center">
-            <p style="height: 85px">
-                <font face="monospace" color="#228B22"><h1><b>Nossa missão</b></h1></font>
+            <p style="height: 50px">
+                <h1><i class="fas fa-users fa-2x" style="color:#f2f2f2"></i></h1>
+                <font face="monospace" color="#9aff01">
+                    <h1><b>Nossa missão</b></h1>
+                </font>
                 <p>
-                    <font face="monospace" color="#f2f2f2">Transformar pessoas, provendo soluções que valorizem nossos clientes</font>
+                    <font face="monospace" color="#f2f2f2">
+                        <h4>Transformar pessoas, provendo soluções <br/> que valorizem nossos clientes. <br/><br/></h4>
+                    </font>
                 </p>
             </p>
         </div>
+    <!-- Coluna da Esquerda -->
         <div style="float:right; padding:2px; text-align:center">
-            <p style="height: 85px">
-                <font face="monospace" color="#228B22"><h1><b>Nossa visão</b></h1></font>
+            <p style="height: 50px">
+                <h1><i class="far fa-gem fa-2x" style="color:#f2f2f2"></i></h1>
+                <font face="monospace" color="#9aff01">
+                    <h1><b>Nossos valores</b></h1>
+                </font>
+            </p>
+            <p>
+                <font face="monospace" color="#f2f2f2">
+                    <h4>Transparência, sinergia, empatia, <br/> responsabilidade social, impacto e orgulo <br/> de ser visionário.</h4>
+                </font>
             </p>
         </div>
+    <!-- Coluna da Meio -->
         <div style="float:center; padding:2px; text-align:center">
-            <p style="height: 85px">
-                <font face="monospace" color="#228B22"><h1><b>Nossos valores</b></h1></font>
+            <p style="height: 50px">
+                <h1><i class="fas fa-chart-line fa-2x" style="color:#f2f2f2"></i></h1>
+                <font face="monospace" color="#9aff01">
+                    <h1><b>Nossa visão</b></h1>
+                </font>
+            </p>
+            <p>
+                <font face="monospace" color="#f2f2f2">
+                    <h4>Ser a empresa referência em soluções <br/> tecnológicas, na região de João <br/> Molevarde e no MEJ mineiro, com um <br/> time forte e determinado.</h4>
+                </font>
             </p>
         </div>
 </div>
+<br>
+<!-- Coluna de Serviços -->
+<div class="container-fluid wrapper">
+    <font face="monospace">
+        <h1 style="font-size:300%;"><b>Nossos Serviços</b></h1> <!--Esta um pouco desalinhado -->
+    </font>
+</div>
+<br>
+<div class="container-fluid wrapper" style="background-color:#2E2E2E; height:350px;">
+    <!-- Coluna da Direita -->
+        <div style="float:left; padding:2px; text-align:center">
+                <p style="height: 50px">
+                    <h1><i class="fas fa-flask fa-2x" style="color:#f2f2f2"></i></h1>
+                    <font face="monospace" color="#9aff01">
+                        <h1><b>Serviço 1</b></h1>
+                    </font>
+                    <p>
+                        <font face="monospace" color="#f2f2f2">
+                            <h4>Nesta sessão será descrito o <br/> serviço realizado</h4>
+                        </font>
+                    </p>
+                </p>
+        </div>
+    <!-- Coluna da Esquerda -->
+        <div style="float:right; padding:2px; text-align:center">
+            <p style="height: 50px">
+                <h1><i class="fas fa-flask fa-2x" style="color:#f2f2f2"></i></h1>
+                <font face="monospace" color="#9aff01">
+                    <h1><b>Serviço 3</b></h1>
+                </font>
+            </p>
+            <p>
+                <font face="monospace" color="#f2f2f2">
+                    <h4>Nesta sessão será descrito o <br/> serviço realizado</h4>
+                </font>
+            </p>
+        </div>
+    <!-- Coluna da Meio -->
+        <div style="float:center; padding:2px; text-align:center">
+            <p style="height: 50px">
+                <h1><i class="fas fa-flask fa-2x" style="color:#f2f2f2"></i></h1>
+                <font face="monospace" color="#9aff01">
+                    <h1><b>Serviço 2</b></h1>
+                </font>
+            </p>
+            <p>
+                <font face="monospace" color="#f2f2f2">
+                    <h4>Nesta sessão será descrito o <br/> serviço realizado</h4>
+                </font>
+            </p>
+        </div>
+</div>
+<br>
+<!-- Botão de Orçamento -->
+<form method="get" action="#" style="text-align:center; font-size:200%;">
+    <!-- Falta estilizar o botão -->
+    <button type="submit">
+        <font face="monospace">Solicite um Orçamento</font>
+    </button>
+</form>
