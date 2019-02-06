64f28fc78ab3a841111840e02f47435f705412a8
gabrielcaetanodm <gabrielcaetanodm@gmail.com>
2019-02-06 18:59:26 -0200

    ajuste do scroll do botao de topo

--- application/views/frontend/template/html-header.php
+++ application/views/frontend/template/html-header.php
@@ -22,6 +22,8 @@
     <link href="<?php echo base_url('assets/frontend/css/backtotop.css') ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/frontend/css/scrollbar.css') ?>" rel="stylesheet">
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
     <!-- Custom Fonts -->
     <link href="<?php echo base_url('assets/frontend/css/font-awesome/css/font-awesome.min.css') ?>" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
