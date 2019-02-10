f04648ef7018a9875b062de7abd451cc8d76f0e1
gabrielcaetanodm <gabrielcaetanodm@gmail.com>
2019-02-10 16:05:27 -0200

    backend servicos, att backend equipe

--- application/models/Servicos_model.php
+++ application/models/Servicos_model.php
@@ -17,4 +17,38 @@ class Servicos_model extends CI_Model {
         $this->db->from('servicos');
         return $this->db->get()->result();
     }
+
+    public function listar_servico($id){
+        $this->db->select('id, nome, descricao, imagem');
+        $this->db->from('servicos');
+        $this->db->where('md5(id)',$id);
+        return $this->db->get()->result();
+    }
+
+	public function adicionar($nome, $descricao, $imagem){
+        $dados['nome'] = $nome;
+        $dados['descricao'] = $descricao;
+        $dados['imagem'] = $imagem;
+
+        return $this->db->insert('servicos',$dados);
+    }
+
+    public function alterar($id, $nome, $descricao){
+        $dados['nome'] = $nome;
+        $dados['descricao'] = $descricao;
+
+        $this->db->where('id',$id);
+        return $this->db->update('servicos',$dados);
+    }
+
+    public function remover($id, $imagem){
+        $this->db->where('md5(id)',$id);
+        return $this->db->delete('servicos');
+    }
+
+    public function nova_foto($id, $imagem){
+    	$dados['imagem'] = $imagem;
+      	$this->db->where('md5(id)',$id);
+      	return $this->db->update('servicos',$dados);
+    }
 }
